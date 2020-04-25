c409aebe24238ca2b81b12be44d1e90d6c5aa6a1
James Q Quick <james.q.quick@outlook.com>
2020-04-24 21:23:20 -0500

    Revert "Fixed minor issues."

--- Final/highscores.js
+++
@@ -1,8 +0,0 @@
-const highScoresList = document.getElementById("highScoresList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
